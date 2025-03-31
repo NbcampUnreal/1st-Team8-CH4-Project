61be678aba6e73dd2e2f69c29804258f161c1ed1
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-03-31 17:33:29 +0900

    [main] Update 기본 프로젝트 소스 경로 정리

---
+++ Source/PlatformFighterStarterKit/T8GameMode.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "T8GameMode.h"
+
---
+++ Source/PlatformFighterStarterKit/T8GameMode.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "T8GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMFIGHTERSTARTERKIT_API AT8GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
