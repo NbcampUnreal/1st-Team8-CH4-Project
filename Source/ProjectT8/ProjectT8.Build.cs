59d3ab6083b22d5e70b2a90f94824ca38edcdaef
IINGS <wjdgns10293@gmail.com>
2025-04-02 20:56:55 +0900

    [jhun-001] Update 버전 5.5로 변경 (initialize)

--- Source/ProjectT8/ProjectT8.Build.cs
+++ Source/ProjectT8/ProjectT8.Build.cs
@@ -5,9 +5,8 @@ public class ProjectT8 : ModuleRules
 	public ProjectT8(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		CppStandard = CppStandardVersion.Cpp20;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
@@ -19,6 +18,6 @@ public class ProjectT8 : ModuleRules
         // Uncomment if you are using online features
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-    }
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
 }
