e299459a988641d89746b07fe759bffafbc11da9
chanmin60 <dlcksals60@gmail.com>
2025-04-02 19:51:56 +0900

    [chanmin-002] Update AIController Perception 코드 작성

--- Source/ProjectT8/AI/T8AIController.cpp
+++ Source/ProjectT8/AI/T8AIController.cpp
@@ -1,2 +1,45 @@
 #include "AI/T8AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISense_Sight.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+AT8AIController::AT8AIController()
+{
+	AIPerception = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerception"));
+    SetPerceptionComponent(*AIPerception);
+
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+	if (SightConfig)
+	{
+		SightConfig->SightRadius = 1000.0f;
+		SightConfig->LoseSightRadius = 1200.0f;
+		SightConfig->PeripheralVisionAngleDegrees = 90.0f;
+		SightConfig->SetMaxAge(5.0f);
+		SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+		SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+		SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+
+		AIPerception->ConfigureSense(*SightConfig);
+		AIPerception->SetDominantSense(SightConfig->GetSenseImplementation());
+	}
+
+	AIPerception->OnTargetPerceptionUpdated.AddDynamic(this, &AT8AIController::OnTargetPerceived);
+}
+
+void AT8AIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AT8AIController::OnTargetPerceived(AActor* Actor, FAIStimulus Stimulus)
+{
+	if (Stimulus.WasSuccessfullySensed())
+	{
+		//이후에 Blackboard에 TargetActor 세팅 예정
+	}
+	else
+	{
+		//마찬가지
+	}
+}
--- Source/ProjectT8/AI/T8AIController.h
+++ Source/ProjectT8/AI/T8AIController.h
@@ -2,14 +2,30 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "T8AIController.generated.h"
 
-/**
- * 
- */
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+
 UCLASS()
 class PROJECTT8_API AT8AIController : public AAIController
 {
 	GENERATED_BODY()
 	
+
+public:
+	AT8AIController();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	UAIPerceptionComponent* AIPerception;
+
+	UPROPERTY()
+	UAISenseConfig_Sight* SightConfig;
+
+	UFUNCTION()
+	void OnTargetPerceived(AActor* ACtor, FAIStimulus Stimulus);
 };
