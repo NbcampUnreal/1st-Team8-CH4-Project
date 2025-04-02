18074170f74e2fd9e9aeb9f8e4b7936d54eb7278
IINGS <142036336+IINGS@users.noreply.github.com>
2025-04-03 02:10:36 +0900

    Revert "Jhun 001 version change to 5.5"

--- Source/ProjectT8Editor.Target.cs
+++ Source/ProjectT8Editor.Target.cs
@@ -6,8 +6,10 @@ public class ProjectT8EditorTarget : TargetRules
 	public ProjectT8EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
-		ExtraModuleNames.Add("ProjectT8");
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		bOverrideBuildEnvironment = true;
+
+		ExtraModuleNames.AddRange( new string[] { "ProjectT8" } );
 	}
 }
