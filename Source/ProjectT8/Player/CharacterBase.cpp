fc9c2280501713346f6f4c38144029597f9c96f5
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-04-04 20:22:54 +0900

    [eunsu-004] Update 걷기, 뛰기 애니메이션 리타겟팅 완료

--- Source/ProjectT8/Player/CharacterBase.cpp
+++ Source/ProjectT8/Player/CharacterBase.cpp
@@ -26,7 +26,7 @@ ACharacterBase::ACharacterBase()
 
 	GetCharacterMovement()->JumpZVelocity = 700.f;
 	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
