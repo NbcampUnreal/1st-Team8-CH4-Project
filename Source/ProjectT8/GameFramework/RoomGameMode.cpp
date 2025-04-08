22670a1e67e86009159dd02276417b8839cfa28f
yoon-park <jiyoonpark.dev@gmail.com>
2025-04-09 06:28:42 +0900

    [jiyoon-001] add: maps에 Room(개인실) 추가
    - 새로운 맵 추가에 따른 GameMode, GameState 추가

---
+++ Source/ProjectT8/GameFramework/RoomGameMode.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "GameFramework/RoomGameMode.h"
+
---
+++ Source/ProjectT8/GameFramework/RoomGameState.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "RoomGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTT8_API ARoomGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
