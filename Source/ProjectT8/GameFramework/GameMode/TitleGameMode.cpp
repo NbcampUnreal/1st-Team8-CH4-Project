94aebf9eb53d052570e589d84b14dbba949ed691
yoon-park <jiyoonpark.dev@gmail.com>
2025-04-15 18:31:20 +0900

    [jiyoon-005] update: TitleScreen 제작 중
    - TitleImage 추가
    - Phase 변경 시 발생하는 버그 수정
    - EGamePhase에 아무것도 지정하지 않은 경우의 None 추가
    - 프로젝트세팅에서 GameInstance 변경

--- Source/ProjectT8/GameFramework/GameMode/TitleGameMode.cpp
+++ Source/ProjectT8/GameFramework/GameMode/TitleGameMode.cpp
@@ -1,5 +1,11 @@
-
-
-
 #include "TitleGameMode.h"
 
+
+ATitleGameMode::ATitleGameMode()
+{
+	//PlayerControllerClass = ATitlePlayerController::StaticClass();
+
+	DefaultPawnClass = nullptr;
+	PlayerStateClass = nullptr;
+	HUDClass = nullptr;
+}
--- Source/ProjectT8/GameFramework/GameState/TitleGameState.h
+++ Source/ProjectT8/GameFramework/GameState/TitleGameState.h
@@ -1,17 +1,17 @@
-
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Common/CustomGameState.h"
 #include "TitleGameState.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class PROJECTT8_API ATitleGameState : public ACustomGameState
 {
 	GENERATED_BODY()
 	
+public:
+	void BeginPlay() override;
+
+	void InitPhaseAfterDelay();
 };
