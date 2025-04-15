2f9291f5b60eadbf14fbbd98046d39d0db8fe815
민지 이 <almj1169@gmail.com>
2025-04-15 18:52:09 +0900

    [minz-004] Update 독투사체 테스트 완료

--- Source/ProjectT8/Item/Throwable.cpp
+++ Source/ProjectT8/Item/Throwable.cpp
@@ -10,10 +10,6 @@ AThrowable::AThrowable()
     PrimaryActorTick.bCanEverTick = true;
     SetReplicates(true);
 
-    ItemMesh->SetSimulatePhysics(true);
-    ItemMesh->SetEnableGravity(true);
-    ItemMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-    ItemMesh->SetCollisionResponseToAllChannels(ECR_Block);
     ItemMesh->OnComponentHit.AddDynamic(this, &AThrowable::OnHit);
 }
 
@@ -26,12 +22,36 @@ void AThrowable::Use(ACharacterBase* Player)
 {
     if (!Player || bIsThrown) return;
 
-    DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+    // 스케일 확인용 로그
+    UE_LOG(LogTemp, Warning, TEXT("Before Detach - Scale: %s"), *GetActorScale3D().ToString());
+
+    FDetachmentTransformRules DetachRules(EDetachmentRule::KeepWorld, true);
+    DetachFromActor(DetachRules);
+
+    // 확실하게 물리 재설정
+    ItemMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    ItemMesh->SetCollisionResponseToAllChannels(ECR_Block);
+    ItemMesh->SetCollisionObjectType(ECC_PhysicsBody);
+
+    ItemMesh->SetSimulatePhysics(true);
+    ItemMesh->SetEnableGravity(true);
+
+    // 스케일 강제 고정 (테스트용)
+    SetActorScale3D(FVector(2.0f));
+
 
     FVector ThrowDirection = Player->GetActorForwardVector() + FVector(0, 0, 0.5f);
     ItemMesh->AddImpulse(ThrowDirection * ThrowForce, NAME_None, true);
 
     bIsThrown = true;
+    /*if (!Player || bIsThrown) return;
+
+    DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+
+    FVector ThrowDirection = Player->GetActorForwardVector() + FVector(0, 0, 0.5f);
+    ItemMesh->AddImpulse(ThrowDirection * ThrowForce, NAME_None, true);
+
+    bIsThrown = true;*/
 }
 
 void AThrowable::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
