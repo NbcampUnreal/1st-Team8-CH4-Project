b18f8e39a0c153b712ad8d1f93c313b3816250dd
IINGS <wjdgns10293@gmail.com>
2025-04-17 00:51:01 +0900

    [jhun-008] Update 게임시작 버튼 눌렀을때 정보 넘기는 로직 변경

--- Source/ProjectT8/Multi/Widget/StartGameButton.cpp
+++ Source/ProjectT8/Multi/Widget/StartGameButton.cpp
@@ -1,7 +1,8 @@
 ﻿#include "Multi/Widget/StartGameButton.h"
 #include "GameFramework/GameState/LobbyGameState.h"
 #include "Components/Button.h"
-
+#include "GameFramework/Common/T8GameInstance.h"
+#include "Kismet/GameplayStatics.h"
 
 void UStartGameButton::NativeConstruct()
 {
@@ -17,13 +18,19 @@ void UStartGameButton::OnStartGameButtonClicked()
     UWorld* World = GetWorld();
     if (World)
     {
-        // 로비 상태 등을 확인하고 게임 시작 조건을 만족하는지 검사...
-        // 예: 모든 플레이어가 준비되었는지, 최소 인원이 충족되었는지 등
+        // 로비 상태의 정보를 GameInstance에 저장
+        ALobbyGameState* LobbyState = World->GetGameState<ALobbyGameState>();
+        UT8GameInstance* GameInstance = Cast<UT8GameInstance>(UGameplayStatics::GetGameInstance(World));
 
-        UE_LOG(LogTemp, Log, TEXT("ServerTravel: Attempting to travel to Gameplay Map..."));
+        if (LobbyState && GameInstance)
+        {
+            // 로비 정보를 GameInstance에 저장
+            GameInstance->SaveLobbyGameState(LobbyState);
+
+            UE_LOG(LogTemp, Log, TEXT("ServerTravel: Saved lobby data and attempting to travel to Gameplay Map..."));
+        }
 
         // bUseSeamlessTravel이 GameMode에서 true로 설정되어 있다고 가정
-
         World->ServerTravel("BattleLevel");
     }
 }
