2bbc8897ec2f63ca180ec0fcb8a1420e6a71be5b
민지 이 <almj1169@gmail.com>
2025-04-23 04:32:15 +0900

    [minz-007] Del 구현 못한 클래스들 삭제

--- Source/ProjectT8/Item/IceGun.cpp
+++
@@ -1,7 +0,0 @@
-#include "Item/IceGun.h"
-
-AIceGun::AIceGun()
-{
-    ItemName = "IceGun";
-    //ProjectileClass = 스턴건 투사체 클래스 지정, 헤더에 스턴건투사체클래스 선언必
-}
--- Source/ProjectT8/Item/IceProjectile.cpp
+++
@@ -1,5 +0,0 @@
-#include "Item/IceProjectile.h"
-
-AIceProjectile::AIceProjectile()
-{
-}
